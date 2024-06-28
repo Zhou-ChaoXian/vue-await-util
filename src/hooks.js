88ba966e6ff8a5e93b6d6c5ad28a0ae53decedf8
Zhou-ChaoXian <2542606900@qq.com>
2024-06-29 00:53:20 +0800

    update

--- src/hooks.js
+++ src/hooks.js
@@ -17,13 +17,24 @@ const resolveStatus = Symbol("resolve");
 const rejectStatus = Symbol("reject");
 
 function useAwait({resolve, init, delay = 300, jumpFirst = false, onStart, onEnd, onError}) {
-  let status = pendingStatus;
+  let status = resolve instanceof Promise ? pendingStatus : resolveStatus;
   let cacheResolve = null;
   let resolveValue = init;
   let first = true;
   const cancelMap = new Map();
   let customTrigger = null;
-  const setStatus = (resolve) => {
+  let resolveData = null;
+
+  function generateResolveData() {
+    resolveData = markRaw({
+      first,
+      status,
+      data: resolveValue,
+      error: cacheResolve instanceof Promise ? Reflect.get(cacheResolve, _error) : undefined,
+    });
+  }
+
+  function setStatus(resolve) {
     if (Reflect.has(resolve, _data)) {
       status = resolveStatus;
       resolveValue = Reflect.get(resolve, _data);
@@ -31,8 +42,9 @@ function useAwait({resolve, init, delay = 300, jumpFirst = false, onStart, onEnd
       status = rejectStatus;
       onError?.(Reflect.get(resolve, _error));
     }
-  };
-  const handle = (resolve) => {
+  }
+
+  function validate(resolve) {
     if (!(resolve instanceof Promise) || cacheResolve === resolve)
       return false;
     if (!Reflect.has(resolve, _tracked)) {
@@ -56,6 +68,7 @@ function useAwait({resolve, init, delay = 300, jumpFirst = false, onStart, onEnd
             onEnd?.(first);
             first = false;
             setStatus(resolve);
+            generateResolveData();
             customTrigger();
           }
         }, delay);
@@ -64,8 +77,10 @@ function useAwait({resolve, init, delay = 300, jumpFirst = false, onStart, onEnd
       cacheResolve = resolve;
       setStatus(resolve);
     }
+    generateResolveData();
     return true;
-  };
+  }
+
   if (jumpFirst) {
     first = false;
     resolve = Object.defineProperties(Promise.resolve(init), {
@@ -73,21 +88,16 @@ function useAwait({resolve, init, delay = 300, jumpFirst = false, onStart, onEnd
       [_data]: {value: init},
     });
   }
-  handle(resolve);
+  validate(resolve);
   return customRef((track, trigger) => {
     customTrigger = trigger;
     return {
       get() {
         track();
-        return markRaw({
-          status,
-          data: resolveValue,
-          error: Reflect.get(cacheResolve, _error),
-          first,
-        });
+        return resolveData;
       },
       set(value) {
-        handle(value) && trigger();
+        validate(value) && trigger();
       }
     };
   });
